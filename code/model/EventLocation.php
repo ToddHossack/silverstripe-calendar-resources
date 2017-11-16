b095af2563dde56f78d70d1a652e57208fbb37a3
Todd Hossack <todd@tiraki.com>
2017-11-16 14:08:14 +1300

    Revisions to admin fields, config and language fields

--- code/model/EventLocation.php
+++ code/model/EventLocation.php
@@ -12,7 +12,7 @@ class EventLocation extends DataObject
 		'State'    => 'Varchar(100)',
 		'Country'  => 'Varchar(100)',
         'Postcode' => 'Varchar(10)',
-        'TimeZone' => 'Varchar(100)', /** @todo */
+        'TimeZone' => 'Varchar(100)',
         'Phone' => 'Varchar(32)'
     );
     
@@ -28,47 +28,144 @@ class EventLocation extends DataObject
 		'FullAddressString' => 'Text'
 	);
     
+    private static $summary_fields = array(
+        'Title',
+        'City',
+        'State',
+        'Country',
+        'UsageCount'
+    );
+    
     /*
 	 * -------------------------------------------------------------------------
 	 * Admin methods
 	 * -------------------------------------------------------------------------
 	 */
-    public function getCMSFields() {
-        $fields = parent::getCMSFields();
-		
-        $tzField = TimeZoneField::create('TimeZone', _t('EventLocation.TimeZone'));
-
-		$fields->replaceField('TimeZone',$tzField);
-
+    public function getCMSFields()
+    {
+        
+        $fields = FieldList::create();
+        $fields->push(TabSet::create("Root", $mainTab = Tab::create("Main")));
+        
+        /*
+         * Main tab
+         */
+        // Title
+        $fields->addFieldsToTab('Root.Main',array(
+            TextField::create('Title',_t('EventLocation.Title','Title')),
+            HtmlEditorField::create('Description',_t('EventLocation.Description','Description'))
+        ));
+       
+        /*
+         * Address tab
+         */
+        $fields->findOrMakeTab('Root.AddressTab',_t('EventLocation.AddressTab','Address'));
+        // Address
+        $fields->addFieldsToTab('Root.AddressTab',array(
+            TextField::create('Address',_t('EventLocation.Address','Street address')),
+            TextField::create('City',_t('EventLocation.City','Suburb / city')),
+            TextField::create('State',_t('EventLocation.State','State / province')),
+            TextField::create('Country',_t('EventLocation.Country','Country')),
+            TextField::create('Postcode',_t('EventLocation.Postcode','Post code')),
+            //TimeZoneField::create('TimeZone', _t('EventLocation.TimeZone')) @todo
+            TextField::create('Phone',_t('EventLocation.Phone','Phone'))
+        ));
+       
+        /*
+         * Usage tab
+         */
+        $fields->findOrMakeTab('Root.UsageTab',_t('EventLocation.UsageTab','Usage'));
+        $eventsList = array();
+        foreach($this->Events() as $event) {
+            $eventsList[] = $event->ID .' '. $event->StartDateTime .' '. $event->Title;
+        }
+        $fields->addFieldToTab('Root.UsageTab',
+            ReadonlyField::create('EventsList',_t('EventLocation.Events','Events'))->setValue(implode("\n",$eventsList))
+        );
+        
         return $fields;
 	}
     
+    public function getAddNewFields() {
+        $fields = $this->scaffoldFormFields(array(
+			'includeRelations' => false,
+			'tabbed' => false,
+			'ajaxSafe' => true
+		));
+		
+        $fields->removeByName('Description');
+        $fields->removeByName('TimeZone');
+        
+        /** @todo
+        $tzField = TimeZoneField::create('TimeZone', _t('EventLocation.TimeZone'));
+		$fields->replaceField('TimeZone',$tzField);
+        */
+        return $fields;
+	}
+    
+    public function singular_name()
+	{
+		return _t('EventLocation.SINGULARNAME', 'Location');
+	}
+
+	public function plural_name()
+	{
+		return _t('EventLocation.PLURALNAME', 'Locations');
+	}
+    
+    public function fieldLabels($includeRelations = true)
+	{
+		return array_merge(
+			parent::fieldLabels($includeRelations),
+			array(
+				'Title' => _t('EventLocation.Title', 'Title'),
+				'Description' => _t('EventLocation.Description', 'Description'),
+				'Address' => _t('EventLocation.Address', 'Address'),
+				'City' => _t('EventLocation.City', 'Suburb / city'),
+				'State' => _t('EventLocation.State', 'State / province'),
+				'Postcode' => _t('EventLocation.Postcode', 'Post code'),
+                'Country' => _t('EventLocation.Country', 'Country'),
+                'TimeZone' => _t('EventLocation.TimeZone', 'Disabled'),
+                'Phone' => _t('EventLocation.Phone', 'Phone'),
+                'UsageCount' => _t('EventLocation.UsageCount', 'Usage count')
+			)
+		);
+	}
     
     /*
 	 * -------------------------------------------------------------------------
-	 * Template methods
+	 * Template methods - @todo
 	 * -------------------------------------------------------------------------
-	 */
+	 
     public function MapHTML($width,$height)
     {
 		$data = ArrayData::create(array(
 			'Width'    => $width,
 			'Height'   => $height,
-			'Address' => rawurlencode($this->getLocationAddressString()),
+			'Address' => rawurlencode($this->getFullAddressString()),
 			'ApiKey' => SiteConfig::current_site_config()->MapsApiKey
 		));
 		return $data->renderWith('LocationMap');
 	}
     
+    
+    public function UsageCount()
+    {
+        return $this->Events()->count();
+    }
+    
+     * 
+     */
+    
     /*
 	 * -------------------------------------------------------------------------
-	 * Helper methods
+	 * Helper methods @todo
 	 * -------------------------------------------------------------------------
 	 */
     /**
 	 * Determines whether object has minimum location fields
 	 * @return boolean
-	 */
+	 
 	public function hasFullAddress()
 	{
 		return ($this->Address
@@ -112,6 +209,6 @@ class EventLocation extends DataObject
 		$str = implode(', ',array_filter($arr));
 		return trim(preg_replace('/\n+/',', ', $str));
 	}
-    
+    */
 }
 
